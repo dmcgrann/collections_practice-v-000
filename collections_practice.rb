73163b4643a49bcd108c4196e96df91574c70466
David McGrann <dpmcgrann@gmail.com>
2018-03-12 22:20:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,5 +31,5 @@ def sum_array(int)
 end
 
 def add_s(word)
-  word.each_with_index.collect{|e, i| next if i === word[1] e + "s"}
+  word.each_with_index.collect{|e, i| next if i == word[1] e + "s"}
 end
